ba4c90c22e927d87d86cd46ccef1939f5cfdc8e3
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2019-10-07 10:46:02 +0200

    Force version v2.0.0 for java 8 compatibility

--- bin/install.sh
+++ bin/install.sh
@@ -80,8 +80,9 @@ PREFIX="$(cd -P -- "${PREFIX}" && pwd)"
 echo "Installing into ${PREFIX}" | sed "s#$HOME#~#g"
 
 mkdir -p ${PREFIX}
-releaseUrl=$(curl --head --silent ${repo_url}/releases/latest | grep "Location:" | cut -c11-)
-releaseTag=$(echo $releaseUrl | awk 'BEGIN{FS="/"}{print $8}' | tr -d '\r')
+#releaseUrl=$(curl --head --silent ${repo_url}/releases/latest | grep "Location:" | cut -c11-)
+#releaseTag=$(echo $releaseUrl | awk 'BEGIN{FS="/"}{print $8}' | tr -d '\r')
+releaseTag=v2.0.0
 url=${repo_url}/releases/download/${releaseTag}/okta-aws-cli-${releaseTag:1}.jar
 dest=${PREFIX}/$(basename ${url})
 echo "Latest release JAR file: ${url}"
